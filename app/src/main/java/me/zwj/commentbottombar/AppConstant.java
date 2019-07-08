406e1d0b9da16712f64f1c32ad12582a047b4d5c
Your Name <zc_program@163.com>
2019-07-08 11:23:08 +0800

    配置Zcrop开源库

---
+++ app/src/main/java/me/zwj/commentbottombar/AppConstant.java
@@ -0,0 +1,82 @@
+package me.zwj.commentbottombar;
+
+/**
+ * Created by Zc on 2017/5/3.
+ */
+
+public class AppConstant {
+    public static final int REQUEST_CODE_PICK_IMAGE_ZERO = 6;
+    public static final int REQUEST_CODE_PICK_IMAGE = 1;
+    public static final int REQUEST_CODE_AVATAR_IMAGE = 2;
+    public static final int REQUEST_CODE_CONTENT = 3;
+    public static final int REQUEST_CODE_MESSAGE = 4;
+    public static final int REQUEST_CODE_VIDEO = 5;
+    public static final String EXTRA_ARGUMENTS = "extra_arguments";
+
+    //传输到发布预览中 头部标题图片
+    public static final String EXTRA_HEADIMAGE = "imagehead";
+
+    //图标
+    public static final String EXTRA_AVATARS = "mAvatars";
+
+    public static final String EXTRA_VIDEO_CONTENT = "extra_content";
+    public static final String EXTRA_CONTENT = "extra_content";
+    //判断加载头部图片样式类型
+    public static final String EXTRA_IMAGE_TYPE = "extra_IMAGETYPE";
+
+    public static final String EXTRA_RELEASR_TYPE = "release_type";
+
+    public static final String EXTRA_TITLE = "extra_title";
+    public static final String EXTRA_ADDRESS = "extra_address";
+    public static final String EXTRA_PHONE = "extra_phone";
+    public static final String EXTRA_PRICE = "extra_price";
+    public static final String EXTRA_UNIT = "extra_unit";
+    public static final String EXTRA_NUMBER = "extra_number";
+    public static final String EXTRA_FREIGHT = "extra_freight";
+    public static final int SAY_IMAGE_MAX = 9;
+    public static final int ARTICLE_IMAGE_MAX = 3;
+    public static final int ARTICLE_COMMENT_IMAGE_MAX = 3;
+
+    //跳转预览的 价格单位库存 数量
+
+
+    /**
+     * 发布首页下的栏目
+     */
+    public static final int RELEASE_HEADLINE = 101;//头条文章
+    public static final int RELEASE_PPICTURE = 102;//图片
+    public static final int RELEASE_HOT = 103;//热点
+    public static final int RELEASE_VIDEO = 104;//视频
+    public static final int RELEASE_PERSION = 105;//人物
+    public static final int RELEASE_STORY = 106;//故事
+    public static final int RELEASE_HELP = 107;//互助
+
+    /**
+     * 发布发现下的栏目
+     */
+    public static final int RELEASE_FIND = 201;//发现秘密
+    public static final int RELEASE_QUCHU = 202;//有去处
+    public static final int RELEASE_NICEGOODS = 203;//好货
+    public static final int RELEASE_SERVICE = 204;//服务
+    public static final int RELEASE_SHOPPING = 205;//逛街
+    public static final int RELEASE_PERYPHERY =206;//周边
+    public static final int RELEASE_ACTIVITY = 207;//活动
+    public static final int RELEASE_SAYSAY = 20;//写说说
+
+    public static final int RELEASE_SAYSAYVIDEO= 21;//发布视频说说
+
+
+    /**
+     * 跳转首页下的栏目
+     */
+    public static final String INTENT_HEADLINE = "101";//头条文章
+    public static final String INTENT_PPICTURE = "102";//图片
+    public static final String INTENT_HOT = "103";//热点
+    public static final String INTENT_VIDEO = "104";//视频
+    public static final String INTENT_PERSION = "105";//人物
+    public static final String INTENT_STORY = "106";//故事
+    public static final String INTENT_HELP = "107";//互助
+
+
+
+}
--- app/src/main/java/me/zwj/commentbottombar/MainActivity.java
+++ app/src/main/java/me/zwj/commentbottombar/MainActivity.java
@@ -5,18 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 import me.pandazhang.filepicker.FilePicker;
 import me.pandazhang.filepicker.activity.ImagePickActivityPicker;
+import me.pandazhang.filepicker.filter.entity.ImageFile;
 
 public class MainActivity extends AppCompatActivity {
     public static final String IS_NEED_CAMERA = "IsNeedCamera";
     public static final int REQUEST_CODE_PICK_IMAGE = 1;
     private Button mButtom;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.iv);
         mButtom=findViewById(R.id.buttom);
         mButtom.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +36,42 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
             }
         });
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case AppConstant.REQUEST_CODE_PICK_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    //处理添加图片的信息
+                    ArrayList<ImageFile> list = data.getParcelableArrayListExtra(FilePicker.RESULT_PICK_IMAGE);
+                    /*for (int i = 0; i < list.size(); i++) {
+                        mRteLayout.insertImage(list.get(i).getPath(), mRteLayout.getMeasuredWidth());
+                        mSaveView.setEnabled(true);
+                    }*/
+                    if (list.size()>0){
+                        Glide.with(this).load(list.get(0).getPath()).into(imageView);
+                    }
+
+                }
+                break;
+            case AppConstant.REQUEST_CODE_PICK_IMAGE_ZERO:
+                if (resultCode == RESULT_OK) {
+                    //处理添加图片的信息
+                    ArrayList<ImageFile> list = data.getParcelableArrayListExtra(FilePicker.RESULT_PICK_IMAGE);
+                    /*for (int i = 0; i < list.size(); i++) {
+                        mRteLayout.insertImage(list.get(i).getPath(), mRteLayout.getMeasuredWidth());
+                        mSaveView.setEnabled(true);
+                    }*/
+                    if (list.size()>0){
+                        Glide.with(this).load(list.get(0).getPath()).into(imageView);
+                    }
+
+                }
+                break;
+        }
     }
 }
