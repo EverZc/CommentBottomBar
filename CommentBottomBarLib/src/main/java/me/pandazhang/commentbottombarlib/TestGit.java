c3c93409d5710357c28f67acef4b5f93e525d687
everzc <7642786@qq.com>
2019-07-10 13:46:54 +0800

    test

--- CommentBottomBarLib/src/main/java/me/pandazhang/commentbottombarlib/TestGit.java
+++ CommentBottomBarLib/src/main/java/me/pandazhang/commentbottombarlib/TestGit.java
@@ -6,5 +6,5 @@ package me.pandazhang.commentbottombarlib;
  * @CreateDate: 19-7-10 上午11:58
  */
 public class TestGit {
-    String a ;
+    String a  ="asdfja;klsjfk ;lasjfk jsakl;fj klasdjf kl;ajsdkf ljsadk;l fjkasl;d jfkal;sd jf kl;asjdfoqjf klasjfkds;ajfkl;asdjfd qwi;a" ;
 }
